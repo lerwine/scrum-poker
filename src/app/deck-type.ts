b00dc350460d65f187216e0afb5cd8d5a5cb5646
Leonard <lenny@erwinefamily.net>
2023-01-13 13:45:09 -0500

    Added image file dimensios interface

--- src/app/deck-type.ts
+++ src/app/deck-type.ts
@@ -1,5 +1,6 @@
 import { CardDefinition } from './card-definition';
 import { SheetDefinition } from './sheet-definition';
+import { ImageFileDimensions } from './image-file-dimensions';
 import { getMinimalFibonacciCardDefinitions, getFibonacciCardDefinitions,
   getExtFibonacciCardDefinitions, getPseudoFibonacciCardDefinitions, getIntegralCardDefinitions } from './card-definition';
 import { getMinimalFibonacciSheetDefinitions, getFibonacciSheetDefinitions,
@@ -9,35 +10,35 @@ export function getDeckTypes(): DeckType[] {
   return [
     {
       name: "Minimal Fibonacci",
-      preview: "MinimalFibonacci-Preview.svg",
+      preview: { uri: "MinimalFibonacci-Preview.svg", width: 210, height: 166 },
       description: "Fibonacci sequence values up to 34.",
       cards: getMinimalFibonacciCardDefinitions(),
       sheets: getMinimalFibonacciSheetDefinitions()
     },
     {
       name: "Fibonacci",
-      preview: "Fibonacci-Preview.svg",
+      preview: { uri: "Fibonacci-Preview.svg", width: 210, height: 176 },
       description: "Fibonacci sequence values up to 89.",
       cards: getFibonacciCardDefinitions(),
       sheets: getFibonacciSheetDefinitions()
     },
     {
         name: "Extended Fibonacci",
-        preview: "ExtFibonacci-Preview.svg",
+        preview: { uri: "ExtFibonacci-Preview.svg", width: 210, height: 187 },
         description: "Fibonacci sequence values up to 233.",
         cards: getExtFibonacciCardDefinitions(),
         sheets: getExtFibonacciSheetDefinitions()
     },
     {
         name: "Pseudo-Fibonacci",
-        preview: "PseudoFibonacci-Preview.svg",
+        preview: { uri: "PseudoFibonacci-Preview.svg", width: 210, height: 165 },
         description: "Estimation values up to 100, similar to Fibonacci sequence.",
         cards: getPseudoFibonacciCardDefinitions(),
         sheets: getPseudoFibonacciSheetDefinitions()
     },
     {
         name: "Integral",
-        preview: "Integral-Preview.svg",
+        preview: { uri: "Integral-Preview.svg", width: 210, height: 176 },
         description: "Estimation scale values from 1 to 10.",
         cards: getIntegralCardDefinitions(),
         sheets: getIntegralFibonacciSheetDefinitions()
@@ -48,7 +49,7 @@ export function getDeckTypes(): DeckType[] {
 export interface DeckType {
   name: string;
   description: string;
-  preview: string;
+  preview: ImageFileDimensions;
   cards: CardDefinition[];
   sheets: SheetDefinition[];
 }
