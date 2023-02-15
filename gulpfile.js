d5a475eddaf7f9ccba08f3bb18e91631566ed185
Leonard <lenny@erwinefamily.net>
2023-02-14 20:37:01 -0500

    Added gulp file

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp');
+var del = require('del');
+var util = require('util');
+var mocha = require('gulp-mocha');
+var gutil = require('gulp-util');
+var ts = require("gulp-typescript");
+var changed = require('gulp-changed');
+
+var tsDistProject = ts.createProject("tsconfig.json");
+var tsTestProject = ts.createProject("tsconfig.spec.json");
+
+gulp.task('cleanDist', function() { return del([tsDistProject.config.compilerOptions.outDir]); });
+
+gulp.task('cleanTest', function() { return del([tsTestProject.config.compilerOptions.outDir]); });
+
+gulp.task('cleanAll', ['cleanDist', 'cleanTest']);
+
+gulp.task('buildDist', function() {
+    return tsDistProject.src()
+        .pipe(tsDistProject())
+        .pipe(gulp.dest(tsDistProject.config.compilerOptions.outDir));
+});
+
+gulp.task('rebuildDist', ['cleanDist'], function() {
+    return tsDistProject.src()
+        .pipe(tsDistProject())
+        .pipe(gulp.dest(tsDistProject.config.compilerOptions.outDir));
+});
+
+gulp.task('buildTest', function() {
+    return tsTestProject.src()
+        .pipe(tsTestProject())
+        .pipe(gulp.dest(tsTestProject.config.compilerOptions.outDir));
+});
+
+gulp.task('rebuildTest', ['cleanTest'], function() {
+    return tsTestProject.src()
+        .pipe(tsTestProject())
+        .pipe(gulp.dest(tsTestProject.config.compilerOptions.outDir));
+});
+
+gulp.task('buildAll', ['buildDist', 'buildTest']);
+
+gulp.task('rebuildAll', ['rebuildDist', 'rebuildTest']);
+
+gulp.task('runTests', function() {
+    return gulp.src(['test/**/+(test.js|test-)*.js'], { read: false })
+      .pipe(mocha({
+        reporter: 'spec'
+      }));
+  });
