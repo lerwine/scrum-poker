cd1cd5d5d5bff3e78209ddda644c7b8b768cb217
Leonard <lenny@erwinefamily.net>
2023-02-23 14:23:58 -0500

    Add files via upload

--- WebApp/Controllers/UserController.cs
+++ WebApp/Controllers/UserController.cs
@@ -35,7 +35,7 @@ public class UserController : ControllerBase
     public async Task<ActionResult<DataContracts.User.AppState.Response>> GetAppState(CancellationToken token = default)
     {
         UserProfile? userProfile = await _context.GetUserProfileAsync(token);
-        if (userProfile == null)
+        if (typeof userProfile == 'undefined')
              return Unauthorized();
         Guid id = userProfile.Id;
         List<Team> teams = await _context.TeamMembers.Where(m => m.UserId == id).Include(m => m.Team).Select(m => m.Team).Include(t => t.Facilitator).ToListAsync(token);
@@ -73,11 +73,54 @@ public class UserController : ControllerBase
     /// </summary>
     /// <returns></returns>
     [HttpGet("TeamState/{id}")]
-    public async Task<ActionResult<DataContracts.User.TeamState.Response>> GetTeamState(Guid id)
+    public async Task<ActionResult<DataContracts.User.TeamState.Response>> GetTeamState(Guid id, CancellationToken token = default)
     {
-        // if (!_tokenService.ValidateAdminTokenString(token))
-        //     return Unauthorized();
-        throw new NotImplementedException();
+        UserProfile? userProfile = await _context.GetUserProfileAsync(token);
+        if (typeof userProfile == 'undefined')
+             return Unauthorized();
+        Team? team = await _context.Teams.Include(t => t.Facilitator).FindAsync(id);
+        if (team is null)
+            return NotFound();
+        Guid userId = userProfile.Id;
+        if (!userProfile.IsAdmin && team.FacilitatorId != userId)
+        {
+            TeamMember tm = await _context.TeamMembers.Where(m => m.TeamId == id && m.UserId == userId).FirstOrDefaultAsync(token);
+            if (tm is null)
+                return Unauthorized();
+        }
+        DataContracts.User.TeamState.Response response = new()
+        {
+            TeamId = team.Id,
+            Facilitator = new UserListItem
+            {
+                UserId = team.Facilitator.Id,
+                DisplayName = team.Facilitator.DisplayName,
+                UserName = team.Facilitator.UserName
+            },
+            Title = team.Title,
+            Description = team.Description
+        };
+        List<PlanningMeeting> meetings = await _context.Meetings.Where(m => m.TeamId == id).Include(m => m.Participants).ToListAsync(token);
+        if (userProfile.IsAdmin)
+            foreach (PlanningMeeting pm in meetings)
+                response.Meetings.Add(new PlanningMeetingListItem
+                {
+                    MeetingId = pm.Id,
+                    Title = pm.Title,
+                    Description = pm.Description,
+                    MeetingDate = pm.MeetingDate
+                });
+        else
+            foreach (PlanningMeeting pm in meetings)
+                if (pm.Participants.Any(p => p.UserId == userId))
+                    response.Meetings.Add(new PlanningMeetingListItem
+                    {
+                        MeetingId = pm.Id,
+                        Title = pm.Title,
+                        Description = pm.Description,
+                        MeetingDate = pm.MeetingDate
+                    });
+        return response;
     }
 
     // GET: api/User/ScrumMeeting/{id}
@@ -86,10 +129,80 @@ public class UserController : ControllerBase
     /// </summary>
     /// <returns></returns>
     [HttpGet("ScrumMeeting/{id}")]
-    public async Task<ActionResult<DataContracts.User.ScrumState.Response>> GetScrumState(Guid id)
+    public async Task<ActionResult<DataContracts.User.ScrumState.Response>> GetScrumState(Guid id, CancellationToken token = default)
     {
-        // if (!_tokenService.ValidateAdminTokenString(token))
-        //     return Unauthorized();
-        throw new NotImplementedException();
+        UserProfile? userProfile = await _context.GetUserProfileAsync(token);
+        if (typeof userProfile == 'undefined')
+             return Unauthorized();
+        Guid userId = userProfile.Id;
+            
+        PlanningMeeting? planningMeeting = await _context.Meetings.Where(m => m.Id == id).Include(m => m.Team).Include(m => m.Initiative).Include(m => m.Epic).Include(m => m.Milestone).Include(m => m.Participants).FirstOrDefaultAsync(token);
+        if (planningMeeting is null)
+            return NotFound();
+        List<Participant> participants = await _context.Participants.Where(p => p.MeetingId == id).Include(p => p.User).ToListAsync(token);
+        Team team = planningMeeting.Team;
+        if (!(userProfile.IsAdmin || team.FacilitatorId == userId || participants.Any(p => p.UserId == userId)))
+            return Unauthorized();
+        UserProfile facilitator;
+        Dictionary<Guid, UserListItem> usersLookedUp = new();
+        usersLookedUp.Add(userId, new()
+        {
+            UserId = userProfile.Id,
+            DisplayName = userProfile.DisplayName,
+            UserName = userProfile.UserName
+        });
+        facilitator = (userId == team.FacilitatorId) ? userProfile : await _context.Profles.FindAsync(team.FacilitatorId);
+        DataContracts.User.ScrumState.Response response = new()
+        {
+            PlannedStartDate = planningMeeting.PlannedStartDate,
+            PlannedEndDate = planningMeeting.PlannedEndDate,
+            Initiative = (planningMeeting.Initiative is null) ? null : new SprintGroupingResponse()
+            {
+                Title = planningMeeting.Initiative.Title,
+                Description = planningMeeting.Initiative.Description,
+                StartDate = planningMeeting.Initiative.StartDate,
+                PlannedEndDate = planningMeeting.Initiative.PlannedEndDate
+            },
+            Epic = (planningMeeting.Epic is null) ? null : new SprintGroupingResponse()
+            {
+                Title = planningMeeting.Epic.Title,
+                Description = planningMeeting.Epic.Description,
+                StartDate = planningMeeting.Epic.StartDate,
+                PlannedEndDate = planningMeeting.Epic.PlannedEndDate
+            },
+            Milestone = (planningMeeting.Milestone is null) ? null : new SprintGroupingResponse()
+            {
+                Title = planningMeeting.Milestone.Title,
+                Description = planningMeeting.Milestone.Description,
+                StartDate = planningMeeting.Milestone.StartDate,
+                PlannedEndDate = planningMeeting.Milestone.PlannedEndDate
+            },
+            CurrentScopePoints = planningMeeting.CurrentScopePoints,
+            Team = new TeamListItem()
+            {
+                TeamId = team.Id,
+                Description = team.Description,
+                FacilitatorId = team.FacilitatorId,
+                Title = team.Title
+            },
+            Facilitator = new()
+            {
+                UserId = facilitator.Id,
+                DisplayName = facilitator.DisplayName,
+                UserName = facilitator.UserName
+            }
+        };
+        foreach (Participant participant in planningMeeting.Particpants)
+            response.Participants.Add(new()
+            {
+                UserId = participant.UserId,
+                DisplayName = participant.User.DisplayName,
+                UserName = participant.User.UserName,
+                SelectedCardId = participant.SelectedCardId,
+                ColorSchemeId = participant.ColorSchemeId,
+                AssignedPoints = participant.AssignedPoints,
+                SprintCapacity = participant.SprintCapacity
+            });
+        return response;
     }
 }
