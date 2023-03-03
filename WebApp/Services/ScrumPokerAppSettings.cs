2773fbeaaaf1777455d1493368ff15c3e5de5016
Leonard <lenny@erwinefamily.net>
2023-03-03 17:38:38 +0000

    Removed obsolete settings

--- WebApp/Services/ScrumPokerAppSettings.cs
+++ WebApp/Services/ScrumPokerAppSettings.cs
@@ -6,9 +6,5 @@ namespace ScrumPoker.WebApp.Services
     public class ScrumPokerAppSettings
     {
         public string? DbFile { get; set; }
-        public string? AdminToken { get; set; }
-        public Collection<CardSettings>? Cards { get; set; }
-        public Collection<SheetSettings>? Sheets { get; set; }
-        public Collection<DeckSettings>? DeckTypes { get; set; }
     }
 }
--- WebApp/Services/SheetSettings.cs
+++
@@ -1,7 +0,0 @@
-namespace ScrumPoker.WebApp.Services;
-
-public class SheetSettings
-{
-    public string URL { get; set; } = "";
-    public int MaxValue { get; set; }
-}
